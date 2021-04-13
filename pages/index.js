84a351fa4fc406603463f5a4e46c79319e1a4d28
iqballbaihaqi <baihaqiiqbal323@gmail.com>
2021-04-13 18:14:56 +0700

    initial next app

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GetCraft | The Creative Industry's Premium Directory</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
